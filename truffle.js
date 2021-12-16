c4c23d353fd30357ea7f4d67853e4bec5c89f748
Gabriel Dias <diasgab@gmail.com>
2021-12-16 10:55:23 +0700

    Remove dotenv dep

--- truffle.js
+++ truffle.js
@@ -1,15 +1,7 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-var HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   networks: {
     development: {
-      //provider: function() {
-      //  return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
-      //},
       host: "127.0.0.1",
       port: 7545,
       network_id: '5777',
